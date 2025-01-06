01049519a9b3a8fa5d20d3832350993dd8e5bb6c
Logan1xl <mbongowilfried02@gmail.com>
2025-01-06 11:10:48 +0100

    Gestion des Notes

---
+++ src/gestion_des_notes/Gestion_des_notes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gestion_des_notes;
+
+/**
+ *
+ * @author Wulfrid MBONGO
+ */
+public class Gestion_des_notes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
